a38e385bcff156009210a55a1476cb21b6324ff8
Bhavesh Patel <bvhpatel@gmail.com>
2025-09-30 13:41:31 -0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+x = 10
+y = 5
+sum_result = x + y
+difference_result = x - y
+product_result = x * y
+quotient_result = x / y
+
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}")
+print(f"Quotient: {quotient_result}")
